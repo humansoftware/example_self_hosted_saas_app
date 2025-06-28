70fd29199bc89c8991a47aa692708a4f3f1cafbc
Marcelo Valle <mvallebr@gmail.com>
2025-06-28 02:07:08 +0100

    Use right database

--- src/app/api/dbtest/route.ts
+++ src/app/api/dbtest/route.ts
@@ -8,7 +8,7 @@ export async function GET() {
         password: process.env.POSTGRES_PASSWORD,
         host: process.env.POSTGRES_HOST || 'localhost',
         port: process.env.POSTGRES_PORT ? parseInt(process.env.POSTGRES_PORT) : 5432,
-        database: process.env.POSTGRES_DB || process.env.POSTGRES_USER,
+        database: process.env.POSTGRES_DB || process.env.POSTGRES_DATABASE || process.env.POSTGRES_USER,
     });
 
     try {
